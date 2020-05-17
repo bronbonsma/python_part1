e93102457b1610b2200b7d17e0c64295ac9ff208
bronbonsma <babonsma@gmail.com>
2020-05-17 15:35:45 +0300

    Exercise 1

---
+++ Exercises/api.py
@@ -0,0 +1,21 @@
+
+from flask import Flask,json
+import requests
+
+app = Flask(__name__)
+
+
+@app.route("/posts/")
+def get_posts():
+   post_response = requests.get("https://jsonplaceholder.typicode.com/posts")
+   posts = post_response.json()
+
+   response = app.response_class(
+       response=json.dumps(posts),
+       status=200,
+       mimetype="application/json"
+   )
+
+   return response
+if __name__ == "__main__":
+    app.run()
